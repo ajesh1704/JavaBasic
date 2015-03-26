99d470b230ce9b25e3da77c29ff6931afa85e58b
ajesh1704 <ajesh.bharadwaj@gmail.com>
2015-03-26 14:08:15 -0400

    v1.2

--- SubclipseDemo/src/DemoPackage/SVNDemo.java
+++ SubclipseDemo/src/DemoPackage/SVNDemo.java
@@ -4,5 +4,6 @@ public class SVNDemo {
 public static void main(String args[]){
 	System.out.println("THis is a demo program");
 	System.out.println("hey wassup dude");
+	System.out.println("This is version 1.3");
 }
 }
