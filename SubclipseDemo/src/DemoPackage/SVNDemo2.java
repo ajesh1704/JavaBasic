4b0de9e5dc92316af57f4a3458bdfef570468e03
ajesh1704 <ajesh.bharadwaj@gmail.com>
2015-06-01 13:10:54 -0400

    Added a clone file of SVNDemo

---
+++ SubclipseDemo/src/DemoPackage/SVNDemo2.java
@@ -0,0 +1,9 @@
+package DemoPackage;
+
+public class SVNDemo {
+public static void main(String args[]){
+	System.out.println("THis is a demo program");
+	System.out.println("hey wassup dude");
+	System.out.println("This is version 1.3");
+}
+}
